cfaae8a55f5bc94ca00e3021eaa008092b2ded05
zhongyi jiang <47438210+jackiechen1@users.noreply.github.com>
2022-07-25 12:21:03 -0600

    add a module check

--- gui.py
+++ gui.py
@@ -1,6 +1,4 @@
 from tkinter import *
-
-import table
 from table import *
 
 ExampleText = """add (8|M0)               r11.0<1>:ud   r7.0<1;1,0>:ud r101.0<1;1,0>:ud
@@ -10,7 +8,7 @@ mov (8|M0)               r1.1<2>:ud   r5.0<1;1,0>:ud
 mov (8|M0)               r13.0<1>:ud   r79.0<4;4,2>:ud
 mov (8|M0)               r16.0<1>:uw   r13.0<8;8,1>:uw"""
 ExampleModeLabels = ["This example shows an operation adding 8 elements of r7 and r101 to r11",
-                     "This example shows moving 16 elements from r127 to r20",
+                     "This example shows moving 16 elements from r127 to r120",
                      "This example shows the destination (r105) located every other element",
                      "This example shows the destination (r1) adding an offset of one elements",
                      "This example shows the source (r79) changing v,w,h",
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from gui import *
 
-
 numberOfBytes = 32  # 32 or 64
-
+def CheckModule():
+    try:
+        import numpy
+    except ImportError:
+        print("numpy is not installed. Using '$pip install numpy' to install the package")
+    try:
+        import tkinter
+    except ImportError:
+        print("tkinter is not installed")
+    print("All modules are installed!")
 if __name__ == '__main__':
+    CheckModule()
     MainGui = GUI()
     MainGui.run()
